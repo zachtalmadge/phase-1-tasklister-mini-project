1ab88c26560555ea561a2c70eb0812bcb25a5cff
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-09-13 12:29:49 -0400

    pass tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    let newToDo = e.target['new-task-description'].value
+
+    document.querySelector("#tasks").append(newToDo)
+  })
 });
